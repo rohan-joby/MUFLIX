04f8a3f5588c933b16a30c23b68f5dd5f814fbd5
Rohan Joby <rohanjobyak@gmail.com>
2021-07-01 16:27:49 +0530

    wip: add persistant navbar with useScroll hook

--- src/components/Layout/Navigation.jsx
+++ src/components/Layout/Navigation.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
-import { NavLink } from "react-router-dom";
+import { NavLink} from "react-router-dom";
 
+import useScroll from "../../hooks/useScroll";
 import classes from "./Navigation.module.css";
 
 const Navigation = () => {
+    const scrolled = useScroll(100);
     return (
-        <nav className={classes.nav}>
-            <NavLink to="/" activeClassName={classes.active}>
+        <nav className={classes[`${scrolled ? "nav__fixed" : "nav"}`]}>
+            <NavLink to="/" exact activeClassName={classes.active}>
                 <h2 className={classes.logo}>MUFLIX</h2>
             </NavLink>
-            <NavLink to="/my-list" activeClassName={classes.active}>
-                <h2>MY LIST</h2>
+            <NavLink to="/mylist" activeClassName={classes.active}>
+                <h2>My List</h2>
             </NavLink>
         </nav>
     )
 }
 
 export default Navigation;
+//{` ${scrolled ? "nav nav__fixed" : "nav"}`}
+
+// `${scrolled ? "nav nav__fixed":"nav"}`
