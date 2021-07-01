b1eff6ec7b51df8257b16157902418fedcfcf474
Rohan Joby <rohanjobyak@gmail.com>
2021-07-01 16:28:04 +0530

    wip: add more endpoints for data

--- src/data/endpoints.js
+++ src/data/endpoints.js
@@ -18,22 +18,22 @@ export const ENDPOINTS = {
     //   title: 'Upcoming',
     //   endpoint: `/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1&region=IN`,
     // },
-    // {
-    //   title: 'Sci-Fi',
-    //   endpoint: `/discover/movie?api_key=${API_KEY}&with_genres=878&with_watch_providers=8&watch_region=IN`,
-    // },
-    // {
-    //   title: 'Drama',
-    //   endpoint: `/discover/movie?api_key=${API_KEY}&with_genres=18&with_watch_providers=8&watch_region=IN`,
-    // },
-    // {
-    //   title: 'Fantasy',
-    //   endpoint: `/discover/movie?api_key=${API_KEY}&with_genres=14&with_watch_providers=8&watch_region=IN`,
-    // },
-    // {
-    //   title: 'Crime',
-    //   endpoint: `/discover/movie?api_key=${API_KEY}&with_genres=80&with_watch_providers=8&watch_region=IN`,
-    // },
+    {
+      title: 'Sci-Fi',
+      endpoint: `/discover/movie?api_key=${API_KEY}&with_genres=878&with_watch_providers=8&watch_region=IN`,
+    },
+    {
+      title: 'Drama',
+      endpoint: `/discover/movie?api_key=${API_KEY}&with_genres=18&with_watch_providers=8&watch_region=IN`,
+    },
+    {
+      title: 'Fantasy',
+      endpoint: `/discover/movie?api_key=${API_KEY}&with_genres=14&with_watch_providers=8&watch_region=IN`,
+    },
+    {
+      title: 'Crime',
+      endpoint: `/discover/movie?api_key=${API_KEY}&with_genres=80&with_watch_providers=8&watch_region=IN`,
+    },
     {
       title: 'Mystery',
       endpoint: `/discover/movie?api_key=${API_KEY}&with_genres=9648&with_watch_providers=8&watch_region=IN`,
@@ -42,46 +42,46 @@ export const ENDPOINTS = {
       title: 'Action',
       endpoint: `/discover/movie?api_key=${API_KEY}&with_genres=28&with_watch_providers=8&watch_region=IN`,
     },
-    // {
-    //   title: 'Comedy',
-    //   endpoint: `/discover/movie?api_key=${API_KEY}&with_genres=35&with_watch_providers=8&watch_region=IN`,
-    // },
-    // {
-    //   title: 'Animation',
-    //   endpoint: `/discover/movie?api_key=${API_KEY}&with_genres=16&with_watch_providers=8&watch_region=IN`,
-    // },
-    // {
-    //   title: 'Adventure',
-    //   endpoint: `/discover/movie?api_key=${API_KEY}&with_genres=12&with_watch_providers=8&watch_region=IN`,
-    // },
-    // {
-    //   title: 'Family',
-    //   endpoint: `/discover/movie?api_key=${API_KEY}&with_genres=10751&with_watch_providers=8&watch_region=IN`,
-    // },
-    // {
-    //   title: 'TV Movie',
-    //   endpoint: `/discover/movie?api_key=${API_KEY}&with_genres=10770&with_watch_providers=8&watch_region=IN`,
-    // },
-    // {
-    //   title: 'Documentary',
-    //   endpoint: `/discover/movie?api_key=${API_KEY}&with_genres=99&with_watch_providers=8&watch_region=IN`,
-    // },
-    // {
-    //   title: 'War',
-    //   endpoint: `/discover/movie?api_key=${API_KEY}&with_genres=10752&with_watch_providers=8&watch_region=IN`,
-    // },
-    // {
-    //   title: 'History',
-    //   endpoint: `/discover/movie?api_key=${API_KEY}&with_genres=36&with_watch_providers=8&watch_region=IN`,
-    // },
-    // {
-    //   title: 'Western',
-    //   endpoint: `/discover/movie?api_key=${API_KEY}&with_genres=37&with_watch_providers=8&watch_region=IN`,
-    // },
-    // {
-    //   title: 'Thriller',
-    //   endpoint: `/discover/movie?api_key=${API_KEY}&with_genres=53&with_watch_providers=8&watch_region=IN`,
-    // },
+    {
+      title: 'Comedy',
+      endpoint: `/discover/movie?api_key=${API_KEY}&with_genres=35&with_watch_providers=8&watch_region=IN`,
+    },
+    {
+      title: 'Animation',
+      endpoint: `/discover/movie?api_key=${API_KEY}&with_genres=16&with_watch_providers=8&watch_region=IN`,
+    },
+    {
+      title: 'Adventure',
+      endpoint: `/discover/movie?api_key=${API_KEY}&with_genres=12&with_watch_providers=8&watch_region=IN`,
+    },
+    {
+      title: 'Family',
+      endpoint: `/discover/movie?api_key=${API_KEY}&with_genres=10751&with_watch_providers=8&watch_region=IN`,
+    },
+    {
+      title: 'TV Movie',
+      endpoint: `/discover/movie?api_key=${API_KEY}&with_genres=10770&with_watch_providers=8&watch_region=IN`,
+    },
+    {
+      title: 'Documentary',
+      endpoint: `/discover/movie?api_key=${API_KEY}&with_genres=99&with_watch_providers=8&watch_region=IN`,
+    },
+    {
+      title: 'War',
+      endpoint: `/discover/movie?api_key=${API_KEY}&with_genres=10752&with_watch_providers=8&watch_region=IN`,
+    },
+    {
+      title: 'History',
+      endpoint: `/discover/movie?api_key=${API_KEY}&with_genres=36&with_watch_providers=8&watch_region=IN`,
+    },
+    {
+      title: 'Western',
+      endpoint: `/discover/movie?api_key=${API_KEY}&with_genres=37&with_watch_providers=8&watch_region=IN`,
+    },
+    {
+      title: 'Thriller',
+      endpoint: `/discover/movie?api_key=${API_KEY}&with_genres=53&with_watch_providers=8&watch_region=IN`,
+    },
   ],
   helpers: {
     searchMovie: `/search/movie?api_key=${API_KEY}&query=_query`,
--- src/data/genre.js
+++ src/data/genre.js
@@ -1,11 +1,80 @@
 export const GENRE = [
   {
+    id: 28,
+    name: "Action",
+  },
+  {
     id: 9648,
     name: "Mystery",
   },
   {
-    id: 28,
-    name: "Action",
+    id: 12,
+    name: "Adventure",
+  },
+  {
+    id: 35,
+    name: "Comedy",
+  },
+  {
+    id: 16,
+    name: "Animation",
+  },
+  {
+    id: 80,
+    name: "Crime",
+  },
+  {
+    id: 99,
+    name: "Documentary",
+  },
+  {
+    id: 10751,
+    name: "Family",
+  },
+  {
+    id: 10752,
+    name: "War",
+  },
+  {
+    id: 36,
+    name: "History",
+  },
+  // {
+  //   id: 27,
+  //   name: "Horror",
+  // },
+  // {
+  //   id: 10402,
+  //   name: "Music",
+  // },
+  {
+    id: 18,
+    name: "Drama",
+  },
+ 
+  // {
+  //   id: 10749,
+  //   name: "Romance",
+  // },
+  {
+    id: 878,
+    name: "Sci-Fi",
+  },
+  {
+    id: 10770,
+    name: "TV Movie",
+  },
+  {
+    id: 53,
+    name: "Thriller",
+  },
+  {
+    id: 14,
+    name: "Fantasy",
+  },
+  {
+    id: 37,
+    name: "Western",
   },
 ];
 
