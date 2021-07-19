09bb9c034bfd532bd924a2e28a6f48716e73d300
Rohan Joby <rohanjobyak@gmail.com>
2021-07-19 17:41:21 +0530

    wip: add logout functionality

--- src/components/Layout/Navigation.jsx
+++ src/components/Layout/Navigation.jsx
@@ -1,14 +1,27 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React, { useState, useContext } from "react";
+import { NavLink, Link } from "react-router-dom";
 
 import useScroll from "../../hooks/useScroll";
 import Search from "./Search";
 
 import classes from "./Navigation.module.css";
 import Muflix from "../../assets/Muflix-logo.PNG";
+import Avatar from "../../assets/Avatar.png"
+import AuthContext from "../../store/auth-context";
 
 const Navigation = () => {
   const scrolled = useScroll(70);
+  const [profileOpen, setProfileOpen] = useState(false);
+  const authCtx = useContext(AuthContext);
+
+  const profileClickHandler = () => {
+    setProfileOpen(prevValue => !prevValue);  
+  }
+  const classList = profileOpen ? `${classes.navprofile} ${classes.navprofile__active}` :`${classes.navprofile}`;
+
+  const signoutHandler = () => {
+    authCtx.logout();
+  }
 
   return (
     <nav className={classes[`${scrolled ? "nav__fixed" : "nav"}`]}>
@@ -23,8 +36,13 @@ const Navigation = () => {
           <h2>My list</h2>
         </NavLink>
       </div>
-      <div>
-        <Search className={classes.secondary__nav}/>
+      <div className={classes.secondary__nav}>
+        <Search />
+        <div className={classList} onClick={profileClickHandler}>
+          <img src={Avatar} alt="user profile" />
+          <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 320 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"></path></svg>
+          {profileOpen && <button type="button" onClick={signoutHandler} className={classes.signout__button}><Link to="/login">Sign Out</Link></button>}
+        </div>
       </div>
     </nav>
   );
--- src/components/Layout/Search.jsx
+++ src/components/Layout/Search.jsx
@@ -22,9 +22,9 @@ const Search = () => {
   return (
     <form onSubmit={searchHandler} className={classes.search}>
       <label htmlFor="query"></label>
-      <input autoComplete="off" type="search" ref={inputRef} id="query" placeholder="Search movies"/>
+      <input className={classes.input} autoComplete="off" type="search" ref={inputRef} id="query" placeholder="Search movies"/>
       <button className={classes.search__btn}>
-        <BsSearch size={25} style={{ fill: "white" }} />
+        <BsSearch size={21} style={{ fill: "white" }} />
       </button>
     </form>
   );
