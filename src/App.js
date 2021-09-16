41cd01caa1d4241c9c425b4ead3fc3c2ec0dc26d
Rohan Joby <rohanjobyak@gmail.com>
2021-09-16 17:48:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, Suspense } from "react";
+import React, { Suspense } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 
 import ProtectedRoute from "./components/Auth/ProtectedRoute";
@@ -16,36 +16,32 @@ const SingleGenrePage = React.lazy(() => import("./pages/SingleGenrePage"));
 const App = () => {
   return (
     <Suspense fallback={<LoadingSpinner />}>
-        <Switch>
-          <Route path="/register" exact>
-            <Register />
-          </Route>
-          <Route path="/login" exact>
-            <Login />
-          </Route>
-          <Route path="/" exact>
-            <HomePage />
-          </Route>
-          <ProtectedRoute path="/mylist" exact>
-            <MyListPage />
-          </ProtectedRoute>
-          {/* <ProtectedRoute path="/movie/:movie" exact>
-            <MovieDetailsPage />
-          </ProtectedRoute> */}
-          <Route path="/movie/:movie" exact>
-            <MovieDetailsPage />
-          </Route>
-          
-          <ProtectedRoute path="/genre/:genre" exact>
-            <SingleGenrePage />
-          </ProtectedRoute>
-          <ProtectedRoute path="/results/:query" exact>
-            <SearchResultsPage />
-          </ProtectedRoute>
-          <Route path="*">
-            <Redirect to="/" />
-          </Route>
-        </Switch>
+      <Switch>
+        <Route path="/register" exact>
+          <Register />
+        </Route>
+        <Route path="/login" exact>
+          <Login />
+        </Route>
+        <Route path="/" exact>
+          <HomePage />
+        </Route>
+        <ProtectedRoute path="/mylist" exact>
+          <MyListPage />
+        </ProtectedRoute>
+        <ProtectedRoute path="/movie/:movie" exact>
+          <MovieDetailsPage />
+        </ProtectedRoute>
+        <ProtectedRoute path="/genre/:genre" exact>
+          <SingleGenrePage />
+        </ProtectedRoute>
+        <ProtectedRoute path="/results/:query" exact>
+          <SearchResultsPage />
+        </ProtectedRoute>
+        <Route path="*">
+          <Redirect to="/" />
+        </Route>
+      </Switch>
     </Suspense>
   );
 };
