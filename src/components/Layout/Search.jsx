ca232e32c6ad0702b9ced1c96f389cf70c013b70
Rohan Joby <rohanjobyak@gmail.com>
2021-09-16 17:50:10 +0530

    wip: misc. changes

--- src/components/Layout/Search.jsx
+++ src/components/Layout/Search.jsx
@@ -1,6 +1,7 @@
 import React, { useRef } from "react";
 import { useHistory } from "react-router-dom";
 import { BsSearch } from "react-icons/bs";
+import useWindowWidth from "../../hooks/useWindowWidth";
 
 import classes from "./Search.module.css";
 
@@ -8,6 +9,7 @@ const Search = () => {
   const inputRef = useRef();
   const history = useHistory();
   let searchFormatted;
+  const width=useWindowWidth() ;
 
   const searchHandler = (event) => {
     event.preventDefault();
@@ -24,7 +26,7 @@ const Search = () => {
       <label htmlFor="query"></label>
       <input className={classes.input} autoComplete="off" type="search" ref={inputRef} id="query" placeholder="Search movies"/>
       <button className={classes.search__btn}>
-        <BsSearch size={21} style={{ fill: "white" }} />
+        <BsSearch size={width>425 ?21:16} style={{ fill: "white" }} />
       </button>
     </form>
   );
